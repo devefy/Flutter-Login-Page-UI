6da88549df6bf569374fd70c3f458973fa4875db
abdi hamid <abdihamidcj@gmail.com>
2019-04-22 13:08:57 +0300

    first commit

---
+++ lib/CustomIcons.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CustomIcons {
+  static const IconData twitter = IconData(0xe900, fontFamily: "CustomIcons");
+  static const IconData facebook = IconData(0xe901, fontFamily: "CustomIcons");
+  static const IconData googlePlus =
+      IconData(0xe902, fontFamily: "CustomIcons");
+  static const IconData linkedin = IconData(0xe903, fontFamily: "CustomIcons");
+}
